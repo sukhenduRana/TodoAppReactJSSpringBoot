13db7637cb31a7ff4d77afea9ff136dbbf7659c1
Sukhendu Rana <sukhendu.rana@googlemail.com>
2019-04-08 09:44:12 +0530

    Initial commit with CRUD operationd for Todo

---
+++ src/main/java/com/todoapp/rest/webservices/restfulwebservices/todo/TodoHardCodedService.java
@@ -0,0 +1,56 @@
+package com.todoapp.rest.webservices.restfulwebservices.todo;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoHardCodedService {
+	
+	private static List<Todo> todos = new ArrayList<Todo>();
+	private static int idCounter = 0;
+	
+	static {
+		todos.add(new Todo(++idCounter, "admin", "Learn ReactJS", new Date(), false));
+		todos.add(new Todo(++idCounter, "admin", "Learn Spring Boot", new Date(), false));
+		todos.add(new Todo(++idCounter, "admin", "Learn Microservices", new Date(), false));
+		todos.add(new Todo(++idCounter, "admin", "Crack interveiew", new Date(), false));
+	}
+	
+	public List<Todo> findAll(){
+		Comparator<Todo> sorted = (t1, t2) -> t1.getId() - t2.getId();
+		return todos.stream().sorted(sorted).collect(Collectors.toList());
+	}
+
+	public Todo deleteById(long id) {
+		Todo todo = findById(id);
+		if(todo!=null)
+			todos.remove(todo);
+		return todo;
+	}
+
+	public Todo findById(long id) {
+		for(Todo todo : todos) {
+			if(todo.getId() == id) {
+				return todo;
+			}
+		}
+		return null;
+	}
+	
+	public Todo save(Todo todo) {
+		if(todo.getId() == -1 || todo.getId() == 0) {
+			todo.setId(++idCounter);
+			todos.add(todo);
+		}else {
+			deleteById(todo.getId());
+			todos.add(todo);
+		}
+		return todo;
+	}
+}
